532fa83b3d6f7a334c12592f069514acc01d3bbe
Christopher Espiritu <christopheredrian@gmail.com>
2018-03-24 09:04:41 +0800

    Added dynamic word add to dictionary

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,76 @@ const express = require('express');
 const router = express.Router();
 const sentiment = require('sentiment');
 const mongoose = require('mongoose');
+const afinn165 = require('../lib/AFINN.json');
 
 // Load word model
 require('../models/Word');
 const Word = mongoose.model('words');
+
+// TODO: Refactor to helpers
+/**
+ * Returns true if the word is in the afin165 list
+ * @param {string} word 
+ */
+const isInAfinn = function (word) {
+	const obj = afinn165;
+	return !!obj[word];
+}
+
+/**
+ * Remove special characters and return an array of tokens (words).
+ * @param  {string} input Input string
+ * @return {array}        Array of tokens
+ */
+const tokenize = function (input) {
+	return input
+		.toLowerCase()
+		.replace(/\n/g, ' ')
+		.replace(/[.,\/#!$%\^&\*;:{}=_`\"~()]/g, '')
+		.split(' ');
+};
+
+/**
+ * Returns true if the word is in the local dictionary
+ * @param {string} word 
+ * @param {function} callback 
+ */
+const isInLocalWords = function (word, callback) {
+	// TODO: Add Promise support
+	const count = Word.find({ 'word': word })
+		.count()
+		.then((res) => {
+			callback(!!res);
+		});
+}
+
+/**
+ * Calls the callback with the result to be true if it's in the local
+ * and afinn dictionary
+ * @param {String} sentence 
+ */
+const addToDictionary = (sentence) => {
+	// TODO: Add Promise support
+	const words = tokenize(sentence);
+	const obj = afinn165;
+
+	words.forEach((word) => {
+		isInLocalWords(word, (isInLocal) => {
+			if (!isInAfinn(word) && !isInLocal) {
+				const newWord = {
+					word: word,
+					dialect: 'unlisted',
+					user: 'unlisted'
+				}
+				new Word(newWord)
+					.save()
+					.then((word) => {
+						console.log('Unlisted word ' + word + ' added');
+					});
+			}
+		});
+	})
+};
 
 router.get('/', (req, res) => {
 	res.render('api/index');
@@ -21,11 +87,13 @@ router.get('/dialects', (req, res) => {
 
 router.get('/sentiment/sentence', (req, res) => {
 	// Add the word list
+	addToDictionary(req.query.sentence)
+
 	Word.find({}, {
-			'word': 1,
-			'score': 1,
-			_id: 0
-		})
+		'word': 1,
+		'score': 1,
+		_id: 0
+	})
 		.then(words => {
 			localDictionary = {};
 			// TODO: use query instead
@@ -55,10 +123,10 @@ router.post('/sentiment/sentences', (req, res) => {
 		let obj = JSON.parse(req.body.sentences);
 		let response = [];
 		Word.find({}, {
-				'word': 1,
-				'score': 1,
-				_id: 0
-			})
+			'word': 1,
+			'score': 1,
+			_id: 0
+		})
 			.then(words => {
 				localDictionary = {};
 				// TODO: use query instead
@@ -66,6 +134,8 @@ router.post('/sentiment/sentences', (req, res) => {
 					localDictionary[word.word] = word.score;
 				});
 				obj.sentences.forEach((sentence) => {
+					// Add unlisted words
+					addToDictionary(sentence)
 					let res = sentiment(sentence, localDictionary);
 					response.push({
 						'sentiment-result': res,
@@ -99,15 +169,15 @@ router.get('/words', (req, res) => {
 
 		search = {
 			$or: [{
-					word: {
-						$regex: '.*' + req.query.word + '.*'
-					}
-				},
-				{
-					dialect: {
-						$regex: '.*' + req.query.word + '.*'
-					}
+				word: {
+					$regex: '.*' + req.query.word + '.*'
 				}
+			},
+			{
+				dialect: {
+					$regex: '.*' + req.query.word + '.*'
+				}
+			}
 			]
 		}
 
@@ -119,14 +189,14 @@ router.get('/words', (req, res) => {
 		if (!!req.query.word) {
 			search = {
 				$and: [{
-						'dialect': req.query.dialect
+					'dialect': req.query.dialect
 
-					},
-					{
-						'word': {
-							$regex: '.*' + req.query.word + '.*'
-						}
+				},
+				{
+					'word': {
+						$regex: '.*' + req.query.word + '.*'
 					}
+				}
 				]
 			}
 		} else {
